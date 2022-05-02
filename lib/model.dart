6708f4d1c63fae7e8c5840da0223a5fc64192b00
Maria Clara <mcfranca0107@gmail.com>
2022-05-02 17:00:36 -0300

    exercicio flutter maps

--- lib/model.dart
+++ lib/model.dart
@@ -1,10 +1,33 @@
 enum Sexo {MASCULINO, FEMININO }
-
+Sexo? strToSexo(String? str){
+  if (str == null) {
+    return null;
+  }
+  switch (str.toUpperCase()) {
+    case 'MASCULINO':
+    case 'M':
+      return Sexo.MASCULINO;
+    case 'FEMININO':
+    case 'F':
+      return Sexo.FEMININO;
+    default:
+      return null;
+  }
+}
 class Pessoa {
   String? nome;
   String? CPF;
   DateTime? nascimento;
   Sexo? sexo;
+
+  Pessoa(this.nome, this.CPF, this.nascimento, this.sexo);
+
+  Pessoa.fromMap(Map<String, dynamic> map) {
+    nome = map['nome'];
+    CPF = map['CPF'];
+    nascimento = DateTime.tryParse(map['nascimento'] ?? 2000-01-01);
+    sexo = strToSexo(map['sexo']);
+  }
 
 int? getIdade(){
   if (nascimento == null){
@@ -26,6 +49,12 @@ int? getIdade(){
 class Programador extends Pessoa{
   double? salario;
 
+  Programador(
+      String? nome, String? CPF, DateTime? nascimento, Sexo? sexo, this.salario) : super(nome, CPF, nascimento, sexo);
+  Programador.fromMap(Map<String, String> map) : super.fromMap(map) {
+    salario = double.tryParse(map['salario'] ?? "0.0");
+  }
+
   @override
 
   String toString() {
@@ -34,14 +63,23 @@ class Programador extends Pessoa{
 }
 
 void mainEntidades(){
-  bool isBoss = true;
 
-  Programador pessoa = new Programador();
-  pessoa.nome = "José";
-  pessoa.CPF = "123.456.789-09";
-  pessoa.nascimento = DateTime(1990, 10, 10);
-  pessoa.sexo = Sexo.MASCULINO;
-  pessoa.salario = isBoss ? 20000.0 : 10000.0;
+  final map = {
+    'nome': 'José',
+  'nascimento': '2000-01-01',
+  'sexo': 'M',
+  'salario': '10000.0'
+  };
 
-  print(pessoa.toString());
+  Pessoa pessoa = Programador.fromMap(map);
+
+
+  // Programador pessoa = new Programador();
+  // pessoa.nome = "José";
+  // pessoa.CPF = "123.456.789-09";
+  // pessoa.nascimento = DateTime(1990, 10, 10);
+  // pessoa.sexo = Sexo.MASCULINO;
+  // pessoa.salario = isBoss ? 20000.0 : 10000.0;
+  //
+  // print(pessoa.toString());
 }
