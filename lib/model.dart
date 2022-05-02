cab0185f9055572b17f6a29db25b853d77afbce3
Maria Clara <mcfranca0107@gmail.com>
2022-05-02 15:10:21 -0300

    exercicio classe programador e idade

--- lib/model.dart
+++ lib/model.dart
@@ -4,29 +4,42 @@ class Pessoa {
   String? nome;
   String? CPF;
   DateTime? nascimento;
-  int? idade;
   Sexo? sexo;
-  double? salario;
-  bool? programador;
+
+int? getIdade(){
+  if (nascimento == null){
+    return null;
+  }
+  final hoje = DateTime.now();
+  final idade = hoje.difference(nascimento!);
+  return idade.inDays ~/ 365;
+}
 
   @override
   String toString() {
-    return "nome=$nome, cpf=$CPF, nascimento=$nascimento, idade=$idade, sexo=$sexo, "
-        "salario=$salario, programador=$programador";
+    return "nome=$nome, cpf=$CPF, nascimento=$nascimento, idade=${getIdade()}, sexo=$sexo";
 
   }
 
 }
 
+class Programador extends Pessoa{
+  double? salario;
+
+  @override
+
+  String toString() {
+    return "nome=$nome, cpf=$CPF, nascimento=$nascimento, idade=${getIdade()}, sexo=$sexo, salario=$salario";
+  }
+}
+
 void mainEntidades(){
-  Pessoa pessoa = new Pessoa();
+  Pessoa pessoa = new Programador();
   pessoa.nome = "José";
   pessoa.CPF = "123.456.789-09";
-  pessoa.nascimento = DateTime(1986, 10, 10);
-  pessoa.idade = 35;
+  pessoa.nascimento = DateTime(1990, 10, 10);
   pessoa.sexo = Sexo.MASCULINO;
-  pessoa.salario = 10000.0;
-  pessoa.programador = true;
+  //pessoa.salario = 10000.0;
 
   print(pessoa.toString());
 }
